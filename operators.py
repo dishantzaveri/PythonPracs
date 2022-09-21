b74ae792a7bf6aea1a98ccdd3a506d23402e57d7
dishantzaveri <zaveridishant@gmail.com>
2022-09-21 08:53:06 +0530

    new

--- operators.py
+++ operators.py
@@ -58,7 +58,34 @@ print(a)
 
 print(2>3 and 3>4)
 
+print(2>3 or 3>4)
 
+print(not 2>3)
+
+#identity
+
+a=10
+b=10
+print(a is b)
+print(a is not b)
+
+#membership
+
+x=[1,2,3,4,5]
+print(1 in x)
+print(1 not in x)
+
+#bitwise
+
+x=10 #00001010
+y=7  #00000111
+
+print(x&y)  #00000010
+print(x|y)  #00001111
+print(x^y)  #00001101
+print(~x)   #11110101
+print(x>>2) #00000010
+print(x<<2) #00000010
 
 
 
