490f7d5e420e60f845ca95eea1fdc65c62fb7c12
dishantzaveri <zaveridishant@gmail.com>
2022-09-22 16:18:44 +0530

    loops

---
+++ factorial.py
@@ -0,0 +1,7 @@
+a=int(input('enter a'))
+
+for i in range(1,a):
+    a=a*i
+print(a)
+
+
---
+++ forloop.py
@@ -0,0 +1,38 @@
+for i in range(1,7):
+    print(i)
+print('bye')    
+
+#display elements of list with for loop
+
+L=["apple","mango","banana","grapes"]
+for i in L:
+    print(i)
+print('bye')
+
+#display elements of tuple with for loop
+
+L=("A","B","C","D"),
+for i in L:
+    print(i)
+print('bye')
+
+#display elements of string with for loop
+
+L="python"
+for i in L:
+    print(i)
+print('bye')
+
+#display elements of dictionary with for loop
+
+D={"A":"apple","B":"banana","C":"cherry"}
+for i in D:
+    print(i)
+print('bye')
+
+#display elements of set with for loop
+
+S={"apple","mango","banana","grapes"}
+for i in S:
+    print(i)
+print('bye')
