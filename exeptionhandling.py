16e119b4991c2f0e84f3c7e9c0f58073037245bf
dishantzaveri <zaveridishant@gmail.com>
2022-10-20 14:27:08 +0530

    exeptionhandling

---
+++ exeptionhandling.py
@@ -0,0 +1,89 @@
+# Exception understanding
+
+# # index error exception
+# list_data = [1, 2, 3, 4, 5]
+# x=  list_data[6]
+
+# # keyboard interrupt error
+# name=input('Enter your name')
+# ctrl + c
+
+# # key error
+# dict_data={'2' : 'two', '4' : 'four', '6' : 'six'}
+# dict_data['5']
+
+# # 6 name error
+# Names = ['Vijay','Dishant','Rohan']
+# name.lower()
+
+# # 7 Attribute error
+# n=None
+# a=n/2
+
+# # 8 TYpeerror
+# names = ['Vijay','Dishant','Rohan']
+# names/2
+
+# # 9 StopIteration
+# list_data = iter([1, 2, 3, 4])
+# print(next(list_data))
+# print(next(list_data))
+# print(next(list_data))
+# print(next(list_data))
+# print(next(list_data))
+
+# # 10 SyntaxError
+# a = 10
+# b = 20
+# if a>b
+#     print("a is large")
+
+# # 11 Excetion handling
+# try:
+#     a,b = eval(input("Enter 2 nos seperated bt comma"))
+#     result = a/b
+#     print("Result is:",result)
+#     if(a==0):
+#         raise RuntimeError()
+# except ZeroDivisionError:
+#     print("Division by zero.")
+# except SyntaxError:
+#     print("Comma Missing.")
+# except RuntimeError:
+#     print("Maybe Meaningless.")
+# except:
+#     print("Somwthing wrong in the input")
+# else:
+#     print("No exceptions")
+# finally:
+#     print("Finally call is evaluated.")
+
+# User-defined exceptions
+
+
+
+class BaseError(Exception):
+    pass
+class HighValueError(Exception):
+    pass
+class LowValueError(Exception):
+    pass
+value = 29
+while(1):
+    try:
+        n=int(input("Enter number:"))
+        if n> value:
+            raise HighValueError
+        elif n < value:
+            raise LowValueError
+    except LowValueError:
+        print("Very Low Value, Give input again")
+        print()
+    except HighValueError:
+        print("Very High value , give input again")
+        print()
+    else:
+        print("Nice!Correct answer")
+        break
+
+
